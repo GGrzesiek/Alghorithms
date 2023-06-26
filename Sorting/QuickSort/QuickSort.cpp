237b31b2d32ff902a910cf5604254be8d6998690
Grzegorz Kryszczuk <kryszczuk.grzegorz_4a@op.pl>
2023-06-26 22:16:35 +0200

    Added QuickSort (not complete)

--- Sorting/QuickSort/QuickSort.cpp
+++ Sorting/QuickSort/QuickSort.cpp
@@ -1,19 +1,27 @@
 #include <cstdlib>
-#include <iostream>
-using namespace std;
 
-void BubbleSort()
+void quicksort(int x,int lewy,int prawy)
 {
-    int n = 10;
-    int tab[n];
-    for(int i=0;i++;i<n)
+    int srodek = int [(lewy+prawy)/2];
+    int piwot = lewy;
+    x[srodek] = x[prawy];
+    int granica = lewy;
+    for(int i=lewy;i++;i<prawy)
     {
-        for(int j=0;j++;j<n-1)
+        if(x[i]<x[piwot])
         {
-            if(tab[j]>tab[j+1])
-            {
-                swap(tab[j],tab[j+1]);
-            }
+            std::swap(x[i],x[granica]);
+            granica++;
         }
+    }
+    x[prawy]=x[granica];
+    x[granica]=piwot;
+    if(lewy<granica-1)
+    {
+        quicksort(x,lewy,granica-1);
+    }
+    if(granica+1<prawy)
+    {
+        quicksort(x,granica+1,prawy);
     }
 }
